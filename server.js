cdb9546b1a70e994d768395b9fea25f3f361389c
Francesc Rosàs <francescrosasbosque@gmail.com>
2011-07-31 01:18:37 +0200

    Initial version (just downloads and shows the image)

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    utils = require('./lib/utils'),
+    request = require('request')
+
+var app = express.createServer()
+
+app.get('/api/transform', function(req, res) {
+    res = new utils.Response(res)
+    utils.download(req.param('url'), function(error, file, _res) {
+        if (res.handleErrorAsUsersFault(error)) return
+        res.response().contentType(_res.header('Content-Type'))
+        res.response().sendfile(file)
+    })
+})
+
+app.listen(8000)

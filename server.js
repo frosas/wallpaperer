7059f1f264135cf7b0cc89b2b939c9f2e233f6c4
Francesc Rosàs <francescrosasbosque@gmail.com>
2011-07-31 02:21:34 +0200

    A proof of concept version of image resizing
    Not for production as it always use the same file to store the resized
    image.

--- server.js
+++ server.js
@@ -3,12 +3,22 @@ var express = require('express'),
 
 var app = express.createServer()
 
-app.get('/api/transform', function(req, res) {
-    res = new utils.Response(res)
-    utils.download(req.param('url'), function(error, file, _res) {
-        if (res.handleErrorAsUsersFault(error)) return
-        res.response().contentType(_res.header('Content-Type'))
-        res.response().sendfile(file)
+app.get('/api/transform', function(request, response) {
+    response = new utils.Response(response)
+    utils.download(request.param('url'), function(error, file) {
+        if (error) return response.handleUserError(error)
+        var resizedFile = '/tmp/resized'
+        require('imagemagick').resize({ 
+            srcPath: file, 
+            dstPath: resizedFile, 
+            quality: '0.95', 
+            width: request.param('width'),
+            height: request.param('height')
+        }, function(error) {
+            if (error) return response.handleServerError(error)
+            response.response().contentType('image/jpeg')
+            response.response().sendfile(resizedFile)
+        })
     })
 })
 

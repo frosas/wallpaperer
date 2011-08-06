076175edf492d23b7132a87f9551d8ca41f7dfbb
Francesc Rosàs <francescrosasbosque@gmail.com>
2011-08-06 19:00:03 +0200

    Extract scripts to its own file

---
+++ public/scripts/main.js
@@ -0,0 +1,21 @@
+$('input[name=width]').val(screen.width)
+$('input[name=height]').val(screen.height)
+
+$('#image-form').submit(function() {
+    var url = '/api/transform?' + $.param({
+        width: $('input[name=width]').val(),
+        height: $('input[name=height]').val(),
+        url: $('input[name=url]').val()
+    })
+    $('#image-placeholder').html(
+        $('<img>').attr({src: url})
+    )
+    return false
+})
+
+$('#search-similiar-button').click(function() {
+    open('http://images.google.com/searchbyimage?' + $.param({
+        image_url: $('#image-form :input[name=url]').val()
+    }))
+})
+

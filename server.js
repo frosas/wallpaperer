eded4c74f047b6d16f163b569048f5118116da29
Francesc Rosàs <francescrosasbosque@gmail.com>
2011-08-07 04:27:12 +0200

    Don't wrap response object until needed

--- server.js
+++ server.js
@@ -29,19 +29,17 @@ app.get('/api/transform', function(request, response) {
     }
 
     var send = function(image, callback) {
-        response.response().contentType('image/jpeg')
-        response.response().end(image)
+        response.contentType('image/jpeg')
+        response.end(image)
         callback()
     }
 
-    // TODO Why response is undefined if moved inside download()?
-    var response = new utils.Response(response)
     notifyStep('Downloading')
     utils.download(request.param('url'), function(error, file) {
-        if (error) return response.handleError(error)
+        if (error) return new utils.Response(response).handleError(error)
         notifyStep('Resizing and cropping')
         image.resizeAndCrop(file, request.param('width'), request.param('height'), function(error, image) {
-            if (error) return response.handleError(error)
+            if (error) return new utils.Response(response).handleError(error)
             notifyStep('Sending')
             send(image, function() {
                 notifyStep('Done')

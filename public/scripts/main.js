e66b115d1dbe9ae0f2cb6f1d694509eaba9dce99
Francesc Rosàs <francescrosasbosque@gmail.com>
2011-08-08 15:13:33 +0200

    Status and image design changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,24 +1,50 @@
+var wallpaperer = {}
+
+wallpaperer.status = (function() {
+    return {
+        set: function(message) {
+            $('#status .content')
+                .empty()
+                .append($('<div class="message">').text(message))
+            $('#status').toggle(message != '')
+        },
+        clear: function() {
+            this.set('')
+        }
+    }
+})()
+
 $('input[name=width]').val(screen.width)
 $('input[name=height]').val(screen.height)
 
 $('#image-form').submit(function() {
+    wallpaperer.status.set('Sending request')
     var url = '/api/transform?' + $.param({
         width: $('input[name=width]').val(),
         height: $('input[name=height]').val(),
         url: $('input[name=url]').val()
     })
-    $('#image-placeholder').html(
-        $('<img>').attr({src: url})
-    )
+    var imagePlaceholder = $('#image-placeholder')
+        .empty()
+        .append($('<img>').attr({src: url})
+            .hide()
+            .load(function() {
+                $(this).show()
+                wallpaperer.status.clear()
+                imagePlaceholder.append($("<p>Save the image by dragging it to your desktop</p>"))
+            })
+            .error(function(event) {
+                wallpaperer.status.set('Error loading image')
+            }))
     return false
 })
 
 $('#search-similiar-button').click(function() {
     open('http://images.google.com/searchbyimage?' + $.param({
-        image_url: $('#image-form :input[name=url]').val()
+        image_url: $('#image-form input[name=url]').val()
     }))
 })
 
 io.connect().on('transform step', function(args) {
-    $('#step-placeholder').text(args.title)
+    wallpaperer.status.set(args.title)
 })
